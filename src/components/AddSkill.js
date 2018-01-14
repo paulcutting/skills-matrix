5195c98f68f919492affbbb36aa168578eef0409
Paul Cutting <paul.cutting@outlook.com>
2018-01-14 20:49:59 +0000

    Adding missing file

---
+++ src/components/AddSkill.js
@@ -0,0 +1,34 @@
+
+import React from 'react';
+import { Button, FormControl, FormGroup } from 'react-bootstrap';
+
+class AddSkill extends React.Component {
+  onSubmit = (e) => {
+    console.log('Add Skill: ' + this.inputSkillName.value);
+    e.preventDefault();
+    let skillName = this.inputSkillName.value;
+
+    if (skillName.length > 0) {
+      this.inputSkillName.value = '';
+      this.props.onAddSkill(skillName);
+    } else {
+      this.inputSkillName.value.focus();
+    }
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.onSubmit}>
+          <FormGroup controlId="formInlineSkillName">
+            <FormControl inputRef={(input) => this.inputSkillName = input} type="text" placeholder="Skill" />
+            <Button bsStyle="primary" type="submit" >Add Skill</Button>
+          </FormGroup>{' '}
+        </form>
+      </div>
+    );
+  }
+};
+
+export default AddSkill;
+
+
